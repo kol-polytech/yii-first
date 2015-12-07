9e4d150d700a63963ad9975a6a8d03493dd0733c
Vladzzz <vlad.vakalyk@gmail.com>
2015-12-07 13:41:34 +0200

    change file.php

--- file.php
+++ file.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,5 +8,7 @@
 
 return array(
     "1" => "first",
-    "2" => "two"
+    "2" => "two",
+    "5" => "five",
+    "6" => "six"
 );

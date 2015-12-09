49a5a21f30f0f169c20920a59547f95e22706815
Vladzzz <vlad.vakalyk@gmail.com>
2015-12-09 13:48:06 +0200

    juva register

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -33,6 +33,7 @@
                         array('label' => 'Home', 'url' => array('/site/index')),
                         array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
                         array('label' => 'Contact', 'url' => array('/site/contact')),
+                        array('label'=>'Register', 'url'=>array('/user/register')),
                         array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
                         array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest),
                         array('label' => 'Article', 'url' => array('/article/index')),

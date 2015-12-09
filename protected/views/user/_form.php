80938dad898ecb76371235dec035bbb089d3dc9b
andozh1997 <hannasy123@gmail.com>
2015-12-09 13:26:21 +0200

    file _form.php

---
+++ protected/views/user/_form.php
@@ -0,0 +1,38 @@
+<div class="form">
+
+    <?php
+    $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'user-form',
+        'enableAjaxValidation' => false,
+    ));
+    ?>
+
+    <p class="note">Поля з <span class="required">*</span> є обов'язковими.</p>
+
+    <?php echo $form->errorSummary($model); ?>
+
+    <div class="row">
+        <?php echo $form->labelEx($model, 'username'); ?>
+        <?php echo $form->textField($model, 'username', array('size' => 30, 'maxlength' => 30)); ?>
+        <?php echo $form->error($model, 'username'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($model, 'email'); ?>
+        <?php echo $form->textField($model, 'email', array('size' => 30, 'maxlength' => 30)); ?>
+        <?php echo $form->error($model, 'email'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($model, 'password'); ?>
+        <?php echo $form->passwordField($model, 'password', array('size' => 30, 'maxlength' => 30)); ?>
+        <?php echo $form->error($model, 'password'); ?>
+    </div>
+
+    <div class="row buttons">
+        <?php echo CHtml::submitButton($model->isNewRecord ? 'Зареєструватися' : 'Save'); ?>
+    </div>
+
+    <?php $this->endWidget(); ?>
+
+</div>

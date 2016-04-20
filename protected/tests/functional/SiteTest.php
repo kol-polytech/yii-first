2c3e9005cc4a3e3ade633953414a0265508cee8d
Yuriy Cheremischuk <yuracher@gmail.com>
2016-04-20 12:02:26 +0300

    Fix Testing
    Settings and functional code

--- protected/tests/functional/SiteTest.php
+++ protected/tests/functional/SiteTest.php
@@ -10,7 +10,7 @@ class SiteTest extends WebTestCase
 
 	public function testContact()
 	{
-		$this->open('?r=site/contact');
+		$this->open('site/contact');
 		$this->assertTextPresent('Contact Us');
 		$this->assertElementPresent('name=ContactForm[name]');
 
@@ -26,22 +26,22 @@ class SiteTest extends WebTestCase
 		$this->open('');
 		// ensure the user is logged out
 		if($this->isTextPresent('Logout'))
-			$this->clickAndWait('link=Logout (demo)');
+			$this->clickAndWait('link=Logout (admin)');
 
 		// test login process, including validation
 		$this->clickAndWait('link=Login');
 		$this->assertElementPresent('name=LoginForm[username]');
-		$this->type('name=LoginForm[username]','demo');
-		$this->click("//input[@value='Login']");
-		$this->waitForTextPresent('Password cannot be blank.');
-		$this->type('name=LoginForm[password]','demo');
-		$this->clickAndWait("//input[@value='Login']");
-		$this->assertTextNotPresent('Password cannot be blank.');
+		$this->type('name=LoginForm[username]','admin');
+		$this->click("//input[@value='Вхід']");
+		$this->waitForTextPresent('Пароль cannot be blank.');
+		$this->type('name=LoginForm[password]','12345');
+		$this->clickAndWait("//input[@value='Вхід']");
+		$this->assertTextNotPresent('Пароль cannot be blank.');
 		$this->assertTextPresent('Logout');
 
 		// test logout process
 		$this->assertTextNotPresent('Login');
-		$this->clickAndWait('link=Logout (demo)');
+		$this->clickAndWait('link=Logout (admin)');
 		$this->assertTextPresent('Login');
 	}
 }
